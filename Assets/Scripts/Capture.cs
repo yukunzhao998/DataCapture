3c7d59631a9be320ffbd7dfe3abb52595141ab21
Yukun Zhao <yukun@YukundeMacBook-Pro.local>
2024-08-30 11:18:32 +0800

    Update button and readme

--- Assets/Scripts/Capture.cs
+++ Assets/Scripts/Capture.cs
@@ -20,6 +20,10 @@ public class Capture : MonoBehaviour
 
     [SerializeField] public Text log;
 
+    [SerializeField] public Button startButton;
+
+    [SerializeField] public Button stopButton;
+
     public ARCameraManager cameraManager;
 
     private int count;
@@ -38,52 +42,25 @@ public class Capture : MonoBehaviour
     private float angleToNorth;
     private Gyroscope gyro;
 
+    bool captureStart = false;
+    bool captureStop = true;
+
     void Start()
     {
+        startButton.onClick.AddListener(TaskStart);
+
+        stopButton.onClick.AddListener(TaskStop);
+
         cameraManager = GameObject.Find("AR Camera").GetComponent<ARCameraManager>();
 
         Application.lowMemory += OnLowMemory;
 
-        time_name = System.DateTime.Now.ToString("s").Replace(":", "_");
-        Directory.CreateDirectory(Application.persistentDataPath + "/" + time_name);
-        //Debug.Log(time_name);
-
-        ARKitFilePath = Application.persistentDataPath + "/" + time_name + "/" + "ARkitPose" + ".txt";
-        if (!File.Exists(ARKitFilePath))
-        {
-            File.CreateText(ARKitFilePath);
-        }
-        GPSFilePath = Application.persistentDataPath + "/" + time_name + "/" + "GPSData" + ".txt";
-        if (!File.Exists(GPSFilePath))
-        {
-            File.CreateText(GPSFilePath);
-        }
-        MagFilePath = Application.persistentDataPath + "/" + time_name + "/" + "MagnetometerData" + ".txt";
-        if (!File.Exists(MagFilePath))
-        {
-            File.CreateText(MagFilePath);
-        }
-        GyroFilePath = Application.persistentDataPath + "/" + time_name + "/" + "GyroscopeData" + ".txt";
-        if (!File.Exists(GyroFilePath))
-        {
-            File.CreateText(GyroFilePath);
-        }
-        AcceFilePath = Application.persistentDataPath + "/" + time_name + "/" + "AccelerometerData" + ".txt";
-        if (!File.Exists(AcceFilePath))
-        {
-            File.CreateText(AcceFilePath);
-        }
 
 #if PLATFORM_ANDROID
-        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
-        {
-            Permission.RequestUserPermission(Permission.FineLocation);
-        }
-        else
-        {
-            locationGrantedAndroid = true;
-            locationIsReady = NativeGPSPlugin.StartLocation();
-        }
+    {
+        locationGrantedAndroid = true;
+        locationIsReady = NativeGPSPlugin.StartLocation();
+    }
 
 #elif PLATFORM_IOS
 
@@ -107,101 +84,149 @@ public class Capture : MonoBehaviour
 
     unsafe void FixedUpdate()
     {
-        if (!cameraManager.TryAcquireLatestCpuImage(out XRCpuImage image))
-            return;
-
-        float pos_x = _camera.transform.position.x;
-        float pos_y = _camera.transform.position.y;
-        float pos_z = _camera.transform.position.z;
-        float ori_w = _camera.transform.rotation.w;
-        float ori_x = _camera.transform.rotation.x;
-        float ori_y = _camera.transform.rotation.y;
-        float ori_z = _camera.transform.rotation.z;
-
-        var conversionParams = new XRCpuImage.ConversionParams
+        if (captureStart == true)
         {
-            // Get the entire image.
-            inputRect = new RectInt(0, 0, image.width, image.height),
+            count = 0;
+            time_name = System.DateTime.Now.ToString("s").Replace(":", "_");
+            Directory.CreateDirectory(Application.persistentDataPath + "/" + time_name);
+            //Debug.Log(time_name);
 
-            outputDimensions = new Vector2Int(image.width, image.height),
+            ARKitFilePath = Application.persistentDataPath + "/" + time_name + "/" + "ARkitPose" + ".txt";
+            if (!File.Exists(ARKitFilePath))
+            {
+                File.CreateText(ARKitFilePath);
+            }
+            GPSFilePath = Application.persistentDataPath + "/" + time_name + "/" + "GPSData" + ".txt";
+            if (!File.Exists(GPSFilePath))
+            {
+                File.CreateText(GPSFilePath);
+            }
+            MagFilePath = Application.persistentDataPath + "/" + time_name + "/" + "MagnetometerData" + ".txt";
+            if (!File.Exists(MagFilePath))
+            {
+                File.CreateText(MagFilePath);
+            }
+            GyroFilePath = Application.persistentDataPath + "/" + time_name + "/" + "GyroscopeData" + ".txt";
+            if (!File.Exists(GyroFilePath))
+            {
+                File.CreateText(GyroFilePath);
+            }
+            AcceFilePath = Application.persistentDataPath + "/" + time_name + "/" + "AccelerometerData" + ".txt";
+            if (!File.Exists(AcceFilePath))
+            {
+                File.CreateText(AcceFilePath);
+            }
 
-            // Choose RGBA format.
-            outputFormat = TextureFormat.RGBA32,
-
-            transformation = XRCpuImage.Transformation.MirrorY
-
-        };
-
-        // See how many bytes you need to store the final image.
-        int size = image.GetConvertedDataSize(conversionParams);
-
-        // Allocate a buffer to store the image.
-        var buffer = new NativeArray<byte>(size, Allocator.Temp);
-
-        // Extract the image data
-        image.Convert(conversionParams, new IntPtr(buffer.GetUnsafePtr()), buffer.Length);
-
-        // The image was converted to RGBA32 format and written into the provided buffer
-        // so you can dispose of the XRCpuImage. You must do this or it will leak resources.
-        image.Dispose();
-
-
-        // At this point, you can process the image, pass it to a computer vision algorithm, etc.
-        // In this example, you apply it to a texture to visualize it.
-
-        // You've got the data; let's put it into a texture so you can visualize it.
-        camTexture = new Texture2D(
-            conversionParams.outputDimensions.x,
-            conversionParams.outputDimensions.y,
-            conversionParams.outputFormat,
-            false);
-
-        camTexture.LoadRawTextureData(buffer);
-        camTexture.Apply();
-        var bytes = camTexture.EncodeToJPG();
-
-        string path = String.Format(Application.persistentDataPath + "/" + time_name + "/" + "image_{0}" + ".jpg", count.ToString().PadLeft(4,'0'));
-        File.WriteAllBytes(path, bytes);
-
-        // orgTexture = rotateTexture(orgTexture,false);
-
-        using (StreamWriter sr = File.AppendText(ARKitFilePath))
-        {
-            sr.Write("image_{0}" + ".jpg ", count.ToString().PadLeft(4, '0'));
-            sr.WriteLine("{0} {1} {2} {3} {4} {5} {6}", pos_x, pos_y, pos_z, ori_w, ori_x, ori_y, ori_z);
+            captureStart = false;
+            captureStop = false;
         }
 
-        gps_device = new Vector3d(NativeGPSPlugin.GetLatitude(), NativeGPSPlugin.GetLongitude(), NativeGPSPlugin.GetAltitude());
-        using (StreamWriter sa = File.AppendText(GPSFilePath))
+        if(captureStop == false)
         {
-            sa.Write("image_{0}" + ".jpg ", count.ToString().PadLeft(4, '0'));
-            sa.WriteLine("{0} {1} {2}", gps_device.x, gps_device.y, gps_device.z);
-        }
+            if (!cameraManager.TryAcquireLatestCpuImage(out XRCpuImage image))
+                return;
 
-        angleToNorth = Input.compass.trueHeading;
-        using (StreamWriter sb = File.AppendText(MagFilePath))
-        {
-            sb.Write("image_{0}" + ".jpg ", count.ToString().PadLeft(4, '0'));
-            sb.WriteLine("{0}", angleToNorth);
-        }
+            float pos_x = _camera.transform.position.x;
+            float pos_y = _camera.transform.position.y;
+            float pos_z = _camera.transform.position.z;
+            float ori_w = _camera.transform.rotation.w;
+            float ori_x = _camera.transform.rotation.x;
+            float ori_y = _camera.transform.rotation.y;
+            float ori_z = _camera.transform.rotation.z;
 
-        Quaternion gyroAttitude = Input.gyro.attitude;
-        using (StreamWriter sc = File.AppendText(GyroFilePath))
-        {
-            sc.Write("image_{0}" + ".jpg ", count.ToString().PadLeft(4, '0'));
-            sc.WriteLine("{0} {1} {2} {3}", gyroAttitude.w, gyroAttitude.x, gyroAttitude.y, gyroAttitude.z);
-        }
+            var conversionParams = new XRCpuImage.ConversionParams
+            {
+                // Get the entire image.
+                inputRect = new RectInt(0, 0, image.width, image.height),
 
-        using (StreamWriter sd = File.AppendText(AcceFilePath))
-        {
-            sd.Write("image_{0}" + ".jpg ", count.ToString().PadLeft(4, '0'));
-            sd.WriteLine("{0} {1} {2}", Input.acceleration.x, Input.acceleration.y, Input.acceleration.z);
-        }
+                outputDimensions = new Vector2Int(image.width, image.height),
 
-        count = count + 1;
-        log.text = "Time: " + count.ToString() + "\n";
+                // Choose RGBA format.
+                outputFormat = TextureFormat.RGBA32,
+
+                transformation = XRCpuImage.Transformation.MirrorY
+
+            };
+
+            // See how many bytes you need to store the final image.
+            int size = image.GetConvertedDataSize(conversionParams);
+
+            // Allocate a buffer to store the image.
+            var buffer = new NativeArray<byte>(size, Allocator.Temp);
+
+            // Extract the image data
+            image.Convert(conversionParams, new IntPtr(buffer.GetUnsafePtr()), buffer.Length);
+
+            // The image was converted to RGBA32 format and written into the provided buffer
+            // so you can dispose of the XRCpuImage. You must do this or it will leak resources.
+            image.Dispose();
+
+
+            // At this point, you can process the image, pass it to a computer vision algorithm, etc.
+            // In this example, you apply it to a texture to visualize it.
+
+            // You've got the data; let's put it into a texture so you can visualize it.
+            camTexture = new Texture2D(
+                conversionParams.outputDimensions.x,
+                conversionParams.outputDimensions.y,
+                conversionParams.outputFormat,
+                false);
+
+            camTexture.LoadRawTextureData(buffer);
+            camTexture.Apply();
+            var bytes = camTexture.EncodeToJPG();
+
+            string path = String.Format(Application.persistentDataPath + "/" + time_name + "/" + "image_{0}" + ".jpg", count.ToString().PadLeft(4, '0'));
+            File.WriteAllBytes(path, bytes);
+
+            // orgTexture = rotateTexture(orgTexture,false);
+
+            using (StreamWriter sr = File.AppendText(ARKitFilePath))
+            {
+                sr.Write("image_{0}" + ".jpg ", count.ToString().PadLeft(4, '0'));
+                sr.WriteLine("{0} {1} {2} {3} {4} {5} {6}", pos_x, pos_y, pos_z, ori_w, ori_x, ori_y, ori_z);
+            }
+
+            gps_device = new Vector3d(NativeGPSPlugin.GetLatitude(), NativeGPSPlugin.GetLongitude(), NativeGPSPlugin.GetAltitude());
+            using (StreamWriter sa = File.AppendText(GPSFilePath))
+            {
+                sa.Write("image_{0}" + ".jpg ", count.ToString().PadLeft(4, '0'));
+                sa.WriteLine("{0} {1} {2}", gps_device.x, gps_device.y, gps_device.z);
+            }
+
+            angleToNorth = Input.compass.trueHeading;
+            using (StreamWriter sb = File.AppendText(MagFilePath))
+            {
+                sb.Write("image_{0}" + ".jpg ", count.ToString().PadLeft(4, '0'));
+                sb.WriteLine("{0}", angleToNorth);
+            }
+
+            Quaternion gyroAttitude = Input.gyro.attitude;
+            using (StreamWriter sc = File.AppendText(GyroFilePath))
+            {
+                sc.Write("image_{0}" + ".jpg ", count.ToString().PadLeft(4, '0'));
+                sc.WriteLine("{0} {1} {2} {3}", gyroAttitude.w, gyroAttitude.x, gyroAttitude.y, gyroAttitude.z);
+            }
+
+            using (StreamWriter sd = File.AppendText(AcceFilePath))
+            {
+                sd.Write("image_{0}" + ".jpg ", count.ToString().PadLeft(4, '0'));
+                sd.WriteLine("{0} {1} {2}", Input.acceleration.x, Input.acceleration.y, Input.acceleration.z);
+            }
+
+            count = count + 1;
+            log.text = "Time: " + count.ToString() + "\n";
+        }
+        
     }
 
+    void TaskStart()
+    {
+        captureStart = true;
+    }
 
-
+    void TaskStop()
+    {
+        captureStop = true;
+    }
 }
---
+++ readme.md
@@ -0,0 +1,69 @@
+# Data Capture App
+
+**Explanation and usage for the application designed for collecting data based on Unity.** 
+
+## Getting Started
+#### Prerequisites
+1. Clone this project
+```git clone https://github.com/yukunzhao998/DataCapture.git```
+2. Install Unity with Android build support / IOS build support (editor version at least 2021.3.8f1 or higher)
+3. Install Xcode if built for IOS.
+
+#### Installation
+Open the ```DataCapture``` project in Unity.
+* IOS
+    >1. File -> Build Settings -> Platform switch to IOS -> Build And Run
+    >2. Open ```Unity-iPhone.xcodeproj``` in Xcode -> Product -> Destination -> IOS Device ->Signing & capacilities -> Run
+* Android
+    > 1. File -> Build Settings -> Platform switch to Android -> Build
+    > 2. Install apk file to device
+
+
+
+## Usage and data format
+When launching for the first time, allow camera permissions and location permissions. Press ```Start``` button to start a new sequence of capturing and ```Stop``` button to stop the sequence of capturing. The Red text shows the number of images that have been captured in this sequence. 
+
+You can adjust the interval time of capturing in ```Build Settings -> Player Settings -> Time -> Fixed Timestamp```. The images and data files described below are stored locally in the device.
+
+We provide txt data files, which include VIO, gyroscope, GPS, accelerometer and magnetometer data respectively. 
+
+* VIO data
+
+  For ARKitPose/ARCorePose.txt poses files:
+
+  ```image_name x y z qw qx qy qz```.
+
+  ```x, y, z``` are coordinates of position,  6DoF poses follow the Unity coordinate system.
+
+* Gyroscope data
+
+  For GyroscopeData.txt files: 
+
+  ```image_name qw qx qy qz```
+
+  The quaternion follows the Unity coordinate system and represents the device's orientation. 
+
+* GPS data
+
+  For GPSData.txt files:
+
+  ```image_name x y z```
+
+  ```x, y, z``` are latitude, longitude and altitude respectively.
+
+* Accelerometer data
+
+  For AccelerometerData.txt files: 
+
+  ```image_name x y z```
+
+  ```x y z``` are last measured linear acceleration of a device in three-dimensional space following the Unity coordinate system.
+
+* Magnetometer data
+
+  For MagnetometerData.txt files:
+
+  ```image_name trueHeading```
+
+  ```trueHeading``` is the heading in degrees relative to the geographic North Pole.
+
